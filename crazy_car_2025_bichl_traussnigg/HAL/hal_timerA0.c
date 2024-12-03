cc82993e18a2e0159f8b4d8276f27691398f6f92
LeosSurfaceWindows <leo@traussnigg.com>
2024-12-03 21:38:25 +0100

    change TimerA0 settings for speed measurement

--- crazy_car_2025_bichl_traussnigg/HAL/hal_timerA0.c
+++ crazy_car_2025_bichl_traussnigg/HAL/hal_timerA0.c
@@ -4,25 +4,13 @@
 void HAL_TimerA0_Init() {
     TA0CTL |= TACLR;
     TA0CTL |= TASSEL__SMCLK; // Set Source to SubMasterClock
-    TA0CTL |= ID__1;
+    TA0CTL |= ID__8;
     TA0CCTL0 |= CM1; // Rising Edge
     TA0CCTL0 &= ~CAP; // Compare Mode
 //    TA1CCTL0 |= OUTMOD_7; // Set Outmode to Reset/Set
     TA0CCTL0 |= CCIE;
 
-//    TA0CCTL1 |= CM1; // Rising Edge
-//    TA0CCTL1 &= ~CAP; // Compare Mode
-//    TA0CCTL1 |= OUTMOD_7; // Set Outmode to Reset/Set
-////    TA1CCTL1 |= CCIE;
-//
-//    TA0CCTL2 |= CM1; // Rising Edge
-//    TA0CCTL2 &= ~CAP; // Compare Mode
-//    TA0CCTL2 |= OUTMOD_7; // Set Outmode to Reset/Set
-////    TA1CCTL2 |= CCIE;
-
-    TA0CCR0 = 7813; // Periodendauer: 60Hz
-//    TA0CCR1 = 0; // Periodendauer: 60Hz
-//    TA0CCR2 = 3550; // Calibration: Center | Max_Right:  4400 | Max_left: 2700 | radius: 30cm
+    TA0CCR0 = 31252; // 31252=100ms/10Hz
 
     TA0CTL |= MC__UP; // Up mode
 
