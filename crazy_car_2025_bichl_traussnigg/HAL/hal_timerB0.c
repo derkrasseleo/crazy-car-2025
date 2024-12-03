a98e27b5e90a98122bcdebe5c945f91bfed7e878
LeosSurfaceWindows <leo@traussnigg.com>
2024-12-03 21:40:55 +0100

    tried fixing timerB0 - some event still causes TIMER0_B1 ISR

--- crazy_car_2025_bichl_traussnigg/HAL/hal_timerB0.c
+++ crazy_car_2025_bichl_traussnigg/HAL/hal_timerB0.c
@@ -1,5 +1,6 @@
 #include "hal_timerB0.h"
 #include "hal_ucs.h"
+#include "hal_gpio.h"
 #include <msp430.h>
 
 void HAL_TimerB0_Init() {
@@ -9,43 +10,37 @@ void HAL_TimerB0_Init() {
     //SMCLK_FREQU/8/4 = 78125
 
     TB0CCTL0 = CCIE | OUTMOD_7; // Set Outmode to Reset/Set
-    TB0CCTL1 = CCIE | OUTMOD_7;
-    TB0CCTL2 = CCIE | OUTMOD_7;
+//    TB0CCTL1 = CCIE | OUTMOD_7;
+//    TB0CCTL2 = CCIE | OUTMOD_7;
 
     TB0CCR0 = 651; // 39063 for 0.5 Hz; // Should be 39062.5
-    TB0CCR1 = 651; // 120Hz
-    TB0CCR2 = 651; // 7813=100ms/10Hz
+//    TB0CCR1 = 651; // 120Hz
+//    TB0CCR2 = 651; // 7813=100ms/10Hz
 
     TB0CTL &= ~TBIFG; // Clear Interrupt flag
 }
 
-//#pragma vector = TIMER0_B0_VECTOR // for CCR0
-//
-//__interrupt void T0_ISR (void) {
-//    if (TB0CCTL0 & CCIFG)
-//    {
-//        TB0CCTL0 &= ~CCIFG;
-//    }
-//}
+#pragma vector = TIMER0_B0_VECTOR // for CCR0
 
-//#pragma vector = TIMER0_B1_VECTOR // for CCR1-6
-//
-//__interrupt void TB0_CCR1_6_ISR (void) {
-//
-//    switch (TB0IV) {
-//        case TB0IV_TBCCR1:
-//            TB0CCTL1 &= ~CCIFG;
-//            break;
-//        case TB0IV_TBCCR2:
-//            LCD_BACKLIGHT_TOGGLE;
-//            // TODO: check if forwards or backwards
-//            speed = (ticks*10)*10; // *10 mm per tick * 10 Hz
-//            speed = (speed + speed_old) >> 1;
-//            speed_old = speed;
-//            ticks = 0;
-//            TB0CCTL2 &= ~CCIFG;
-//            break;
-//        default:
-//            break;
-//    }
-//}
+__interrupt void T0_ISR (void) {
+    if (TB0CCTL0 & CCIFG)
+    {
+        TB0CCTL0 &= ~CCIFG;
+    }
+}
+
+#pragma vector = TIMER0_B1_VECTOR // for CCR1-6
+
+__interrupt void TB0_CCR1_6_ISR (void) {
+
+    switch (TB0IV) {
+        case TB0IV_TBCCR1:
+            TB0CCTL1 &= ~CCIFG;
+            break;
+        case TB0IV_TBCCR2:
+            TB0CCTL2 &= ~CCIFG;
+            break;
+        default:
+            break;
+    }
+}
