29798aaca0bcb20f0d1f67bebd4ae7836b3ea904
leo <leo@traussnigg.com>
2024-12-26 20:02:26 +0100

    change steer to unsigned char

--- crazy_car_2025_bichl_traussnigg/algorithm/algo.h
+++ crazy_car_2025_bichl_traussnigg/algorithm/algo.h
@@ -1,6 +1,6 @@
 #ifndef ALGORITHM_ALGO_H_
 #define ALGORITHM_ALGO_H_
 
-void primitive_driving(int *perc_steer, int *perc_throttle, unsigned int front_sensor, unsigned int left_sensor, unsigned int right_sensor);
+void primitive_driving(unsigned char *perc_steer, signed char *perc_throttle, unsigned int front_sensor, unsigned int left_sensor, unsigned int right_sensor);
 
 #endif /* ALGORITHM_ALGO_H_ */
