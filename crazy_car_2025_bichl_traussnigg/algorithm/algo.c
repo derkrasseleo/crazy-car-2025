ebc7ff7f0dbd00be5f5e0f7684429f384ab147de
leo <leo@traussnigg.com>
2025-01-03 22:48:34 +0100

    add stuck state

--- crazy_car_2025_bichl_traussnigg/algorithm/algo.c
+++ crazy_car_2025_bichl_traussnigg/algorithm/algo.c
@@ -1,18 +1,21 @@
 #include "algo.h"
 
-enum {STOP, FORWARD, BACKWARDS, LEFT, RIGHT};
+enum {STOP, FORWARD, BACKWARDS, LEFT, RIGHT, STUCK};
 int state = FORWARD;
 int lr_diff;
 int front_old = 0;
 int front_sen_diff;
 extern int speed;
 extern int vbat;
+int cnt_driving = 0;
 
 void primitive_driving(unsigned char *perc_steer, signed char *perc_throttle, unsigned int front_sensor, unsigned int left_sensor, unsigned int right_sensor)
 {
     lr_diff = left_sensor - right_sensor;
     front_sen_diff = front_sensor-front_old;
     front_old = front_sensor;
+
+    cnt_driving++;
 
     if(front_sen_diff<0)
     {
@@ -32,6 +35,12 @@ void primitive_driving(unsigned char *perc_steer, signed char *perc_throttle, un
     if(((front_sensor <= 40) && (left_sensor <= 40)) || ((front_sensor <= 40) && (right_sensor <= 45)))
     {
         state = BACKWARDS;
+    }
+
+    if(speed <= 1 && cnt_driving >= 1000)
+    {
+        cnt_driving = 0;
+        state = STUCK;
     }
 
     switch(state)
@@ -89,6 +98,9 @@ void primitive_driving(unsigned char *perc_steer, signed char *perc_throttle, un
                {
                   state = FORWARD;
                }
+            break;
+        case STUCK:
+            state = BACKWARDS;
             break;
         default:
 
