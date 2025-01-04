efe2d04e47dd1848b5f7796be16160d93c99989a
leo <leo@traussnigg.com>
2025-01-04 17:47:56 +0100

    add more steering range

--- crazy_car_2025_bichl_traussnigg/DL/driver_aktorik.c
+++ crazy_car_2025_bichl_traussnigg/DL/driver_aktorik.c
@@ -8,8 +8,8 @@ const int MaxBreak = (MinFPW+MinRPW)/2;
 
 volatile int speed_controller_impuls = 0;
 
-const int steer_min = 2700;
-const int steer_max = 4300;
+const int steer_min = 2700-200;
+const int steer_max = 4300+200;
 int steer_pc = (steer_max-steer_min)/100;
 
 int mc_val;
