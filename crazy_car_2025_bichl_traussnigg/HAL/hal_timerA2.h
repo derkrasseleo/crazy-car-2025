aa5e4c8a1e0697c05f2f2c7c0629124bb35c3344
leotfh <leo.traussnigg@edu.fh-joanneum.at>
2025-01-15 14:31:06 +0100

    implemented timer A2

---
+++ crazy_car_2025_bichl_traussnigg/HAL/hal_timerA2.h
@@ -0,0 +1,13 @@
+/*
+ * hal_timerA2.h
+ *
+ *  Created on: Jan 15, 2025
+ *      Author: leo
+ */
+
+#ifndef HAL_HAL_TIMERA2_H_
+#define HAL_HAL_TIMERA2_H_
+
+void HAL_TimerA2_Init();
+
+#endif /* HAL_HAL_TIMERA2_H_ */
