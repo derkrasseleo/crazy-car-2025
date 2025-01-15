edce6c2f8377ab37b4da30f7d0135696230a7813
leotfh <leo.traussnigg@edu.fh-joanneum.at>
2025-01-15 14:35:18 +0100

    Add timerA2 init

--- crazy_car_2025_bichl_traussnigg/HAL/hal_general.c
+++ crazy_car_2025_bichl_traussnigg/HAL/hal_general.c
@@ -21,6 +21,7 @@ void HAL_Init() {
     HAL_UCS_Init();
     HAL_TimerB0_Init();
     HAL_TimerA1_Init();
+    HAL_TimerA2_Init();
     HAL_TimerA0_Init();
     HAL_USCIB1_Init();
     HAL_ADC12_Init();
